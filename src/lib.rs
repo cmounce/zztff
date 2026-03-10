93339881c5183a8b170770093a9cf88bc00aeda9
Chris Mounce <christophermounce@gmail.com>
2026-03-09 20:34:07 -0700

    Clean up element.rs
    A lot of this is deleting zztext-specific functionality, such as string
    mappings that go beyond official names for things. It's okay, zztext
    will cope.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod encoding;
 mod error;
 mod parse;
 
-pub use elements::{Element, element_id_from_name, element_name, resolve_alias};
+pub use elements::Element;
 pub use encoding::{decode_multiline, decode_oneline, encode_multiline, encode_oneline};
 pub use error::ParseError;
 pub use parse::{Board, Program, Stat, Tile, World};
